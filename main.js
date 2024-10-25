e709f49ad0c00874f0d440fceb53d0ff9cc2aa28
Elcin Mammadov <elcinelcin57@gmail.com>
2024-10-25 14:10:14 +0400

    git test1

---
+++ main.js
@@ -0,0 +1,90 @@
+//? 1.Task: Yazı(string) uzunluğunu tapın.
+
+// const text = " Salam Dunya";
+// const result = text.length;
+// console.log(result);
+
+//? 2.Task: Yazını böyük hərflərlə göstərin.
+
+// const text = "azərbaycan";
+// const result = text.toUpperCase();
+// console.log(result);
+
+//? 3.Task: Array-de müəyyən bir hissəni kəsin.
+
+// const array = [1, 2, 3, 4, 5];
+// resultArray = array.slice(1, 4);
+// console.log(resultArray);
+
+//? 4.Task: Arrayin sonuna element əlavə edin.
+
+// const array = [1, 2, 3, 4, 5];
+// array.push(6);
+// console.log(array);
+
+//? 5.Task: Arrayin  sonundakı elementi silin.
+
+// const array = [1, 2, 3, 4, 5];
+// array.pop();
+// console.log(array);
+
+//? 6.Task: Arrayin  əvvəlindən elementi silin.
+
+// const array = [1, 2, 3, 4, 5];
+// array.shift();
+// console.log(array);
+
+//? 7.Task: Arrayin əvvəlinə element əlavə edin.
+
+// const array = [1, 2, 3, 4, 5];
+// array.unshift(0);
+// console.log(array);
+
+//? 9.Task: Array-dəki elementləri birləşdirin.
+
+// const array = [5, 12, 8, 130, 44];
+// const resultArray = array.find((num) => num > 10);
+// console.log(resultArray);
+
+//? 9.Task: Array-dəki elementləri birləşdirin.
+
+// const array = [1, 2, 3, 4, 5];
+// const resultArray = array.reduce((acc, cur) => acc + cur, 0);
+// console.log(resultArray);
+
+//? 10.Task: Array-i sıralayın.
+
+// const array = [3, 1, 5, 2, 4];
+// array.sort();
+// console.log(array);
+
+//? 11. Task: Array-də müəyyən bir dəyərin olub olmadığını yoxlayın.
+
+// const array = [1, 2, 3, 4, 5];
+// resultArray = array.includes(2);
+// console.log(resultArray);
+
+//? 12.Task: Stringin müəyyən bir hissəsini yoxlayın.
+
+// const string = "Salam Dunya";
+
+// resultString = string.includes("Dunya");
+// console.log(resultString);
+
+//? 13.Task: Array elementlərini birləşdirin.
+
+// const array = ["Salam", "Dunya"];
+// const resultArray = array.join(" ");
+// console.log(resultArray);
+
+//? 14.Task: Arrayda filtr tətbiq edin.
+
+// const array = [1, 2, 3, 4, 5, 6, 13, 1];
+// const doubleNum = array.filter((num) => num % 2 === 0);
+// console.log(doubleNum);
+
+//? 15. Task:  array-dəki elementləri dəyişdirin.
+
+// const array = [1, 2, 3, 4];
+// const doubleArray = array.map((num) => num * 2);
+// console.log(doubleArray);
